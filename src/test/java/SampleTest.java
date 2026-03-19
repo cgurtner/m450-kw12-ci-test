e67235bf541870718c968ef05a72703490efe415
Cyrill Gurtner <me@cgurtner.ch>
2026-03-19 18:32:05 +0100

    first commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+    @Test
+    void firstTestRuns() {
+        assertEquals(3, 3);
+    }
+}
